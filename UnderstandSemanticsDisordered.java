12fd9d2ecb5ef0275bbe1bfc59ddf65b9adebf9d
WZJun <128673164+OSshiyanGuoGuoGuo@users.noreply.github.com>
2024-08-06 10:13:25 +0800

    Add files via upload

---
+++ UnderstandSemanticsDisordered.java
@@ -0,0 +1,19 @@
+public class UnderstandSemanticsDisordered{
+
+    public static class Reptile {
+        private String name;
+
+        public String getName() {
+            return this.name;
+        }
+    }
+
+    public static class Fish {
+        private String name;
+
+        public String getName() {
+            return this.name;
+        }
+    }
+
+}
